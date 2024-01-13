29db44808b8df6f5ae4438e042693b3831abf226
Bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-13 09:50:16 +0100

    feat(middleware): declare custom middleware

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,67 @@
 import NextAuth from 'next-auth'
 
-import authConfig from './auth.config'
+import authConfig from '@/auth.config'
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  authRoutes,
+  privateRoutes,
+  publicRoutes,
+  apiAuthPrefix,
+} from '@/routes'
 
 const { auth } = NextAuth(authConfig)
 
-export default auth((req) => {})
+export default auth((req) => {
+  const { nextUrl } = req
+  const isLoggedIn = !!req.auth
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix)
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname)
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname)
+  const isPrivateRoute = privateRoutes.includes(nextUrl.pathname)
+
+  if (isApiAuthRoute) {
+    // public for everyone and next-auth
+    return null
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      // stop logged in user from visiting the auth routes
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl))
+    }
+    return null
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+    return Response.redirect(
+      new URL(`/auth/sign-in?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    )
+  }
+
+  if (!isLoggedIn && isPrivateRoute) {
+    let callbackUrl = nextUrl.pathname
+    if (nextUrl.search) {
+      callbackUrl += nextUrl.search
+    }
+
+    const encodedCallbackUrl = encodeURIComponent(callbackUrl)
+
+    return Response.redirect(
+      new URL(`/auth/sign-in?callbackUrl=${encodedCallbackUrl}`, nextUrl)
+    )
+  }
+
+  return null
+})
 
 export const config = {
   matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
