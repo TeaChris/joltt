7d86022686514a7b35f0616a37e6546379e0cca5
Bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-14 14:56:36 +0100

    feat(sidebar): added mock sidebar

--- app/(dashboard)/_component/Sidebar.tsx
+++ app/(dashboard)/_component/Sidebar.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link'
+
 export function Sidebar() {
-  return <div></div>
+  return (
+    <aside className="h-full border-r flex flex-col overflow-y-auto bg-white  shadow-sm">
+      <div className="p-6 flex items-center gap-2 h-[80px] border-b border-b-neutral-200">
+        <Link href="/" className="text-2xl font-extrabold text-primary">
+          jolt.
+        </Link>
+      </div>
+      <div className="flex flex-col w-full">
+        {/* TODO: ADDED SIDEBAR ROUTES */}
+      </div>
+    </aside>
+  )
 }
