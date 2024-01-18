67431f5a6abb49c1ad2a3bbb5e33254eed71fa2d
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-18 12:30:15 +0100

    feat(auth): added callbacks for authentication

--- app/(dashboard)/(admin)/admin/product/page.tsx
+++ app/(dashboard)/(admin)/admin/product/page.tsx
@@ -1,9 +1,17 @@
+import { currentRole, currentUserId } from '@/lib/auth'
 import { ProductForm } from './_component/form'
 import { db } from '@/lib/db'
-import { RoleGate } from '@/components/auth/role-gate'
 import { UserRole } from '@prisma/client'
+import { redirect } from 'next/navigation'
 
 export default async function ProductPage() {
+  const userId = await currentUserId()
+  const role = await currentRole()
+
+  if (!userId || role !== UserRole.ADMIN) {
+    return redirect('/')
+  }
+
   const categories = await db.category.findMany({
     orderBy: {
       name: 'asc',
