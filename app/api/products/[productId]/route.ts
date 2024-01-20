6204b95f447fa175239a9b69e9e92df6ada94920
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-20 13:43:34 +0100

    feat(productId): created api for deleting products

--- app/api/products/[productId]/route.ts
+++ app/api/products/[productId]/route.ts
@@ -3,6 +3,43 @@ import { db } from '@/lib/db'
 import { UserRole } from '@prisma/client'
 import { NextResponse } from 'next/server'
 
+export async function DELETE(
+  req: Request,
+  { params }: { params: { productId: string } }
+) {
+  try {
+    const userId = await currentUserId()
+    const role = await currentRole()
+    const { productId } = params
+
+    if (!userId || role !== UserRole.ADMIN) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const product = await db.products.findUnique({
+      where: {
+        id: productId,
+        userId,
+      },
+    })
+
+    if (!product) {
+      return new NextResponse('Not found', { status: 404 })
+    }
+
+    const deleted = await db.products.delete({
+      where: {
+        id: productId,
+      },
+    })
+
+    return NextResponse.json(deleted)
+  } catch (error) {
+    console.log('[PRODUCT_ID_DELETE]', error)
+    return new NextResponse('Internal Error', { status: 500 })
+  }
+}
+
 export async function PATCH(
   req: Request,
   { params }: { params: { productId: string } }
