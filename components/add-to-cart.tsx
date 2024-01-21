1ce8eaedef8921b65becce37405de6562d92ca09
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-21 16:44:49 +0100

    feat(cart-button): added add-to-cart button logic

--- components/add-to-cart.tsx
+++ components/add-to-cart.tsx
@@ -1,16 +1,35 @@
 'use client'
 
+import { Products } from '@prisma/client'
+import { Button } from './ui/button'
+import { useCart } from '@/hooks/use-cart'
+import { useEffect, useState } from 'react'
+
 interface Props {
   product: Products
 }
 
-import { Products } from '@prisma/client'
-import { Button } from './ui/button'
-
 export function AddToCartButton({ product }: Props) {
+  const { addItem } = useCart()
+  const [isSuccess, setIsSuccess] = useState<boolean>(false)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setIsSuccess(false)
+    }, 2000)
+
+    return () => clearTimeout(timeout)
+  }, [isSuccess])
   return (
-    <Button size="lg" className="w-full">
-      Added
+    <Button
+      onClick={() => {
+        addItem(product)
+        setIsSuccess(true)
+      }}
+      size="lg"
+      className="w-full"
+    >
+      {isSuccess ? 'Added!' : 'Add to cart'}
     </Button>
   )
 }
