a5e8ddde3d0abc457160884a228de693466e8d79
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-21 16:42:53 +0100

    feat(cart): added hook to manage cart

---
+++ hooks/use-cart.ts
@@ -0,0 +1,36 @@
+import { Products } from '@prisma/client'
+
+import { create } from 'zustand'
+import { createJSONStorage, persist } from 'zustand/middleware'
+
+export type CartItem = {
+  product: Products
+}
+
+type CartState = {
+  items: CartItem[]
+  addItem: (product: Products) => void
+  removeItem: (productId: string) => void
+  clearCart: () => void
+}
+
+export const useCart = create<CartState>()(
+  persist(
+    (set) => ({
+      items: [],
+      addItem: (product) =>
+        set((state) => {
+          return { items: [...state.items, { product }] }
+        }),
+      removeItem: (id) =>
+        set((state) => ({
+          items: state.items.filter((item) => item.product.id !== id),
+        })),
+      clearCart: () => set({ items: [] }),
+    }),
+    {
+      name: 'cart-storage',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+)
