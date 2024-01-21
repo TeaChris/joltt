a95005c3103edc511a711cf003463115236eb5bf
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-01-21 18:09:23 +0100

    feat(product): completed product gape

--- app/(collection)/(routes)/products/page.tsx
+++ app/(collection)/(routes)/products/page.tsx
@@ -1,7 +1,45 @@
+import { getProducts } from '@/actions/get-products'
 import { auth } from '@/auth'
+import { MaxWidthWrapper } from '@/components/MaxWidthWrapper'
+import { Categories } from '@/components/categories'
+import { ProductList } from '@/components/product-list'
+import { SearchInput } from '@/components/search-input'
+import { currentUserId } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { redirect } from 'next/navigation'
 
-export default async function Page() {
-  const session = await auth()
+interface Props {
+  searchParams: { title: string; categoryId: string }
+}
 
-  return <div></div>
+export default async function Page({ searchParams }: Props) {
+  const userId = await currentUserId()
+
+  if (!userId) {
+    return redirect('/')
+  }
+
+  const categories = await db.category.findMany({
+    orderBy: {
+      name: 'asc',
+    },
+  })
+
+  // @ts-ignore
+  const products = await getProducts({ userId, ...searchParams })
+
+  return (
+    <MaxWidthWrapper className="space-y-4">
+      <div className="px-6 pt-6 w-full flex items-center justify-center">
+        <SearchInput />
+      </div>
+
+      <div className="w-full flex items-center justify-center">
+        <Categories items={categories} />
+      </div>
+
+      {/* @ts-ignore */}
+      <ProductList items={products} title="Browse high quality collections" />
+    </MaxWidthWrapper>
+  )
 }
---
+++ components/search-input.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import { Search } from 'lucide-react'
+
+import { Input } from './ui/input'
+
+import { useEffect, useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
+
+import { useDebounce } from '@/hooks/use-debounce'
+
+import qs from 'query-string'
+
+export function SearchInput() {
+  const [value, setValue] = useState<string>('')
+  const debouncedValue = useDebounce(value)
+
+  const searchParams = useSearchParams()
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const currentCategoryId = searchParams.get('categoryId')
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: pathname,
+        query: {
+          categoryId: currentCategoryId,
+          title: debouncedValue,
+        },
+      },
+      { skipEmptyString: true, skipNull: true }
+    )
+    router.push(url)
+  }, [debouncedValue, currentCategoryId, router, pathname])
+
+  return (
+    <div className="relative">
+      <Search className="h-4 w-4 absolute top-3 left-3 text-slate-600" />
+      <Input
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="w-full md:w-[650px] pl-9 rounded-full bg-slate-100 focus-visible:ring-slate-200"
+        placeholder="Search products"
+      />
+    </div>
+  )
+}
