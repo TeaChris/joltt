9bdb50dbfabee0b92eaf2985d950e0637d44a4c9
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-02-01 15:24:44 +0100

    feat(thank-you): intuitive ui for thank-you page

--- app/(collection)/(routes)/products/thank-you/page.tsx
+++ app/(collection)/(routes)/products/thank-you/page.tsx
@@ -1,3 +1,4 @@
+import { MaxWidthWrapper } from '@/components/MaxWidthWrapper'
 import { currentUser } from '@/lib/auth'
 import { redirect } from 'next/navigation'
 
@@ -7,5 +8,14 @@ export default async function Page() {
   if (!user) {
     return redirect('/')
   }
-  return <div>Thank You!</div>
+  return (
+    <MaxWidthWrapper className="mt-10 sm:mt-20">
+      <div className="w-1/2 mx-auto space-y-4 flex flex-col items-center">
+        <h1 className="text-3xl sm:text-6xl font-bold">Thank You!</h1>
+        <p className="text-base font-semibold">
+          Your order is now being processed for delivery
+        </p>
+      </div>
+    </MaxWidthWrapper>
+  )
 }
