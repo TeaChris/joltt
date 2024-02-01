fe624e07d6ae2430091d07ad66392393891e8c73
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-02-01 15:37:45 +0100

    feat(thank-you): intuitive ui for thank-you page

--- app/(collection)/(routes)/products/thank-you/page.tsx
+++ app/(collection)/(routes)/products/thank-you/page.tsx
@@ -1,5 +1,6 @@
 import { MaxWidthWrapper } from '@/components/MaxWidthWrapper'
 import { currentUser } from '@/lib/auth'
+import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export default async function Page() {
@@ -9,10 +10,16 @@ export default async function Page() {
     return redirect('/')
   }
   return (
-    <MaxWidthWrapper className="mt-10 sm:mt-20">
-      <div className="w-1/2 mx-auto space-y-4 flex flex-col items-center">
-        <h1 className="text-3xl sm:text-6xl font-bold">Thank You!</h1>
-        <p className="text-base font-semibold">
+    <MaxWidthWrapper className="my-10 sm:my-20">
+      <div className="w-1/2 mx-auto space-y-3 flex flex-col items-center">
+        <Image
+          src={'/thank-you.png'}
+          alt="thank-you image"
+          width={250}
+          height={250}
+          className="object-cover"
+        />
+        <p className="text-xl font-semibold">
           Your order is now being processed for delivery
         </p>
       </div>
