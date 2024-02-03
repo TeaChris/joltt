64d40dadb6a0c97e4b865338fff33a612b45b36c
bermuda <olasunkanmiboluwatife110@gmail.com>
2024-02-03 10:40:33 +0100

    feat(sign-in): fixed sign-in error

--- actions/sign-in.ts
+++ actions/sign-in.ts
@@ -31,6 +31,8 @@ export const logIn = async (
       password,
       redirectTo: DEFAULT_LOGIN_REDIRECT, //callbackUrl ||
     })
+
+    return { success: 'Signed in successfully' }
   } catch (error) {
     if (error instanceof AuthError) {
       switch (error.type) {
